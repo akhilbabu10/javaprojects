2b02a8c60cda6d2c59b1c7402679e522e61d5fc9
Vishnu <44140759+vishnudk@users.noreply.github.com>
2019-09-23 10:36:27 +0530

    Add files via upload

--- gui1.java
+++ gui1.java
@@ -1,3 +1,4 @@
+package swings;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -5,6 +6,7 @@ import java.awt.event.ActionListener;
 //int s1;
 public class gui1 implements  ActionListener
 {
+	
     static JFrame jf_intro=new JFrame();
     static JFrame jf=new JFrame("Registration Form");
     static String cb1[]= {" ","M","F","other"};
@@ -16,8 +18,36 @@ public class gui1 implements  ActionListener
     static JTextField t_moters_name=new JTextField(8);
     static JTextField t_house_name=new JTextField(8);
     static JComboBox j_sex=new JComboBox(cb1);
+    static JTextField t_pass_word=new JTextField();
+    static JTextField t_user_name=new JTextField();
+    static JLabel l_pass_word=new JLabel("id");
+    static JLabel l_user_name=new JLabel("name");
+    static JButton j_ok=new JButton("OK");
+    static JButton make_new=new JButton("make new");
     public static void main(String[]args)
     {
+    	 new_frame();
+    }
+    public static void new_frame()
+    {
+        jf.dispose();
+        jf_intro.setLayout(new GridLayout(6,0,1,1));
+        jf_intro.add(l_user_name);
+        jf_intro.add(t_user_name);
+        jf_intro.add(l_pass_word);
+        jf_intro.add(t_pass_word);
+        jf_intro.add(j_ok);
+        jf_intro.add(make_new);
+        gui1 g=new gui1();
+        make_new.addActionListener(g);
+        j_ok.addActionListener(g);
+        jf_intro.setSize(400,200);
+        jf_intro.setVisible(true);
+        jf_intro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void f_frame()
+    {
+    	jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
         gui1 g1=new gui1();
         jf_intro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf_intro.setBackground(Color.RED);
@@ -26,10 +56,10 @@ public class gui1 implements  ActionListener
         //jf_intro.setBackground(Color.darkGray);
         jf.add(new JLabel("                                                                                                       Registration Form"));
         String cb[]= {"a1","a2","a3"};
-// jf.add(new JComboBox(cb));
+        // jf.add(new JComboBox(cb));
         JButton jb=new JButton("reset");
         JButton jb2=new JButton("ok");
-// jf.add(new JTextField(8));
+        // jf.add(new JTextField(8));
         jf.setLayout(new GridLayout(19, 0,1 , 1));
         // Add labels and text fields to the frame
         jf.add(new JLabel("First Name"));
@@ -38,7 +68,7 @@ public class gui1 implements  ActionListener
         // jf.add(new JComboBox(cb));
         jf.add(t_l_name);
         JLabel l_phone_number=new JLabel("phone number");
-       // l_phone_number.setBackground(new ImageIcon("photo_2019-07-10_23-59-04"));
+        // l_phone_number.setBackground(new ImageIcon("photo_2019-07-10_23-59-04"));
         jf.add(l_phone_number);
         jf.add(t_p_number);
         jf.add(new JLabel("DOB"));
@@ -62,13 +92,11 @@ public class gui1 implements  ActionListener
         //jf.setOpacity();
         jf.setBackground(Color.blue);
         l1.setBackground(Color.green);
-      //  System.out.println(c);
+        //  System.out.println(c);
         System.out.println(t_f_name.getText());
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         String response=e.getActionCommand();
@@ -76,7 +104,8 @@ public class gui1 implements  ActionListener
         {
             System.out.println("ok is being pressed");
             String sex= String.valueOf(j_sex.getItemAt(j_sex.getSelectedIndex()));
-          //  customer c1=new customer(t_father_name.getText(),t_f_name.getText(),t_l_name.getText(),t_moters_name.getText(),t_house_name.getText(),sex,Integer.valueOf(t_dob.getText()),Integer.valueOf(t_p_number.getText()));
+            //  customer c1=new customer(t_father_name.getText(),t_f_name.getText(),t_l_name.getText(),t_moters_name.getText(),t_house_name.getText(),sex,Integer.valueOf(t_dob.getText()),Integer.valueOf(t_p_number.getText()));
+           jf.dispose();
         }
         else if(response=="reset")
         {
@@ -90,14 +119,18 @@ public class gui1 implements  ActionListener
             t_p_number.setText("");
             j_sex.setSelectedItem(" ");
             //jf.dispose();
-            new_frame();
         }
-    }
-    public void new_frame()
-    {
-        jf.dispose();
-        jf_intro.setSize(400,400);
-     jf_intro.setVisible(true);
+        else if(response=="make new")
+        {
+        	f_frame();
+        	jf_intro.dispose();
+        	System.out.println("test1243");
+        	
+        }
+        else if(response=="OK")
+        {
+         jf_intro.dispose();
+        }
     }
 }
 class customer
@@ -115,5 +148,4 @@ class customer
         this.house_name=house_name;
         this.sex=sex;
     }
-
 }
